0a2010aaf70bc6c41bb5dfcd89432bb5419d5af0
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2019-05-01 16:01:00 +0100

    Store local user password in SSM
    The initial need for the story is to allow self-updating pipelines from
    inside the shared concourse.
    
    In order for these users to be useful, we need to expose the password to
    the pipeline so it could use it where appropriate.
    
    Concourse is configured to read secrets from the SSM under certain path
    and allow reading them from inside team pipelines.

--- reliability-engineering/terraform/modules/concourse-keys/local-users.tf
+++ reliability-engineering/terraform/modules/concourse-keys/local-users.tf
@@ -4,6 +4,20 @@ resource "random_string" "local_user_password" {
   length  = 32
 }
 
+resource "aws_ssm_parameter" "concourse_local_usernames_and_passwords" {
+  count = "${length(var.worker_team_names)}"
+
+  name        = "/${var.deployment}/concourse/pipelines/${element(var.worker_team_names, count.index)}/local_user_password"
+  type        = "SecureString"
+  description = "Password for the local user with admin access to team ${element(var.worker_team_names, count.index)}"
+  value       = "${element(random_string.local_user_password.*.result, count.index)}"
+  key_id      = "${aws_kms_key.concourse_worker_shared.key_id}"
+
+  tags = {
+    Deployment = "${var.deployment}"
+  }
+}
+
 output "local_user_passwords" {
   value = "${
     zipmap(
