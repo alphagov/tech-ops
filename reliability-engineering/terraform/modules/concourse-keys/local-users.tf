1769cea9e461e9e546b6050e1e180d6589ca797b
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-05-13 10:01:10 +0100

    Make local user password readonly
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- reliability-engineering/terraform/modules/concourse-keys/local-users.tf
+++ reliability-engineering/terraform/modules/concourse-keys/local-users.tf
@@ -8,7 +8,7 @@ resource "random_string" "local_user_password" {
 resource "aws_ssm_parameter" "concourse_local_usernames_and_passwords" {
   count = "${length(var.worker_team_names)}"
 
-  name        = "/${var.deployment}/concourse/pipelines/${element(var.worker_team_names, count.index)}/local_user_password"
+  name        = "/${var.deployment}/concourse/pipelines/${element(var.worker_team_names, count.index)}/readonly_local_user_password"
   type        = "SecureString"
   description = "Password for the local user with admin access to team ${element(var.worker_team_names, count.index)}"
   value       = "${element(random_string.local_user_password.*.result, count.index)}"
