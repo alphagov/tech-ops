d6da31a19bda7780435179473e163f20aea12f04
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-30 16:41:44 +0100

    Add inline policies to cover trusted advisor and sts permissions missing from SecurityAudit canned policy

---
+++ cyber-security/modules/gds_security_audit_role/inline_policies.tf
@@ -0,0 +1,27 @@
+data "aws_iam_policy_document" "support_inline_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["support:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "support_inline_policy" {
+  name    = "${var.prefix}GDSSecurityAuditInlineSupportPolicy"
+  role    = "${aws_iam_role.gds_security_audit_role.id}"
+  policy  = "${data.aws_iam_policy_document.support_inline_policy_document.json}"
+}
+
+data "aws_iam_policy_document" "sts_inline_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sts:GetCallerIdentity"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "sts_inline_policy" {
+  name    = "${var.prefix}GDSSecurityAuditInlineSupportPolicy"
+  role    = "${aws_iam_role.gds_security_audit_role.id}"
+  policy  = "${data.aws_iam_policy_document.sts_inline_policy_document.json}"
+}
