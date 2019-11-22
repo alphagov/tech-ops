ba051462e56f18670e378d5b51c306f36d92b796
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-11-22 11:12:57 +0000

    Add HTTP to HTTPS redirect for web-page-test
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- reliability-engineering/terraform/modules/web-page-test-cluster/ec2.tf
+++ reliability-engineering/terraform/modules/web-page-test-cluster/ec2.tf
@@ -177,6 +177,22 @@ resource "aws_lb_listener" "web_page_test_controller" {
   }
 }
 
+resource "aws_lb_listener" "web_page_test_controller_http_redirect" {
+  load_balancer_arn = "${aws_lb.web_page_test_controller.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_route53_record" "web_page_test" {
   name    = "${var.subdomain}.${var.domain}"
   type    = "A"
@@ -199,7 +215,7 @@ resource "null_resource" "web_page_test_controller_provisioning" {
 
   triggers = {
     locations_ini_content = "${sha1(file("${path.module}/files/ec2_locations.ini"))}"
-    instance_id = "${aws_instance.web_page_test_controller.id}"
+    instance_id           = "${aws_instance.web_page_test_controller.id}"
   }
 
   provisioner "file" {
