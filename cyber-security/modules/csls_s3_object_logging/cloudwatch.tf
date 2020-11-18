292e8fc06761143f39c68f18391e563fbddb6c39
alice-carr <alice.bc.carr@gmail.com>
2020-11-18 08:17:06 +0000

    pass vars to json file

--- cyber-security/modules/csls_s3_object_logging/cloudwatch.tf
+++ cyber-security/modules/csls_s3_object_logging/cloudwatch.tf
@@ -1,5 +1,5 @@
 data "template_file" "event_pattern" {
-  template = file("${path.module}/json/cloudwatch_event_pattern.json")
+  template = file("${path.module}/json/cloudwatch_event_pattern.tmpl")
 }
 
 resource "aws_cloudwatch_event_rule" "s3_events" {
