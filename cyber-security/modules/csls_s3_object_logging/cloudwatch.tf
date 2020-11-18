14e2399102b3adee5206b1b4b447ef4f5286d243
alice-carr <alice.bc.carr@gmail.com>
2020-11-18 12:44:21 +0000

    add jsonencoding

--- cyber-security/modules/csls_s3_object_logging/cloudwatch.tf
+++ cyber-security/modules/csls_s3_object_logging/cloudwatch.tf
@@ -5,7 +5,7 @@ resource "aws_cloudwatch_event_rule" "s3_events" {
   tags = merge(local.tags, map("Name", local.cloudwatch_event_rule_name))
   
   event_pattern = templatefile("${path.module}/json/cloudwatch_event_pattern.json.tpl", {
-    bucket_arn = var.bucket_arn
+    bucket_arn = jsonencode(var.bucket_arn)
   })
 }
 
