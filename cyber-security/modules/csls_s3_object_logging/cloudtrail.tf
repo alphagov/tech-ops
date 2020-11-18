6604885334ac3a0c15a3665feca293ecfaf14162
alice-carr <alice.bc.carr@gmail.com>
2020-11-18 12:39:14 +0000

    pass through correct vault to cloudtrail

--- cyber-security/modules/csls_s3_object_logging/cloudtrail.tf
+++ cyber-security/modules/csls_s3_object_logging/cloudtrail.tf
@@ -11,7 +11,7 @@ resource "aws_cloudtrail" "s3_object_logging_trail" {
 
     data_resource {
       type   = "AWS::S3::Object"
-      values = var.bucket_arn_list
+      values = [join("", ["arn:aws:s3:::", var.bucket_arn, "/"])]
     }
   }
 }
